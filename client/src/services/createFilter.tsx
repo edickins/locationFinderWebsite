380b04d9494f83918ba2861c552a1ea2299750dc
ecdickins <ed.dickins@bleepbloop.net>
2023-10-20 00:45:35 +0100

    genericised var names

--- client/src/services/createFilter.tsx
+++ client/src/services/createFilter.tsx
@@ -1,8 +1,8 @@
 type FilterFunction<T> = (item: T) => boolean;
 
 function createFilter<T>(filterFunction: FilterFunction<T>) {
-  return function filterToilets(toilets: T[]) {
-    return toilets.filter(filterFunction);
+  return function filterItems(items: T[]) {
+    return items.filter(filterFunction);
   };
 }
 
